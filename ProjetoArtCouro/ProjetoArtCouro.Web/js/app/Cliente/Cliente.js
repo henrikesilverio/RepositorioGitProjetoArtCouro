fc0b230fde0c4bb82342a499998be41ca07f626f
henrikesilverio <henrikesilverio@gmail.com>
2015-08-19 16:35:03 -0300

    Funções js novo cliente

---
+++ ProjetoArtCouro/ProjetoArtCouro.Web/js/app/Cliente/Cliente.js
@@ -0,0 +1,19 @@
+﻿$.extend(Portal, {
+    NovoCliente: function () {
+        //Função para mostrar os esconder dados pessoa fisica ou juridica
+        $("#PessoaFisica").on("change", function() {
+            if (this.value === "true") {
+                $("#DadosPessoaFisica").show("slow");
+                $("#DadosPessoaJuridica").hide("slow");
+            } else {
+                $("#DadosPessoaFisica").hide("slow");
+                $("#DadosPessoaJuridica").show("slow");
+            }
+        });
+        $("#PessoaFisica[value=\"true\"]")[0].checked = true;
+        $("#PessoaFisica").trigger("change");
+    },
+    EditarCliente: function () {
+        
+    }
+});
