4fc517fe757e55c4a544f067f13d433812f326b0
henrique-PC\henrique <henrikesilverio@gmail.com>
2015-08-25 07:18:38 -0300

    Novos Coversores

---
+++ ProjetoArtCouro/ProjetoArtCouro.Api/AutoMapper/EnderecoConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ProjetoArtCouro.Domain.Models.Pessoas;
+using ProjetoArtCouro.Model.Models.Cliente;
+
+namespace ProjetoArtCouro.Api.AutoMapper
+{
+    public class EnderecoConverter : ITypeConverter<ICollection<Endereco>, EnderecoModel>
+    {
+        public EnderecoModel Convert(ResolutionContext context)
+        {
+            var listaEndereco = (ICollection<Endereco>)context.SourceValue;
+            return Mapper.Map<EnderecoModel>(listaEndereco.FirstOrDefault(x => x.Principal));
+        }
+    }
+}
