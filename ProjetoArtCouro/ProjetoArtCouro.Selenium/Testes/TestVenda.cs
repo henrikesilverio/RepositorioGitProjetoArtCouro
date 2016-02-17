80baebd24602e158e94cd7d06a644484b2accf09
henrikesilverio <henrikesilverio@gmail.com>
2016-02-17 09:49:03 -0200

    Teste caixa preta automatizados venda

---
+++ ProjetoArtCouro/ProjetoArtCouro.Selenium/Testes/TestVenda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ProjetoArtCouro.Selenium.TesteSeleniumBrowsers;
+using ProjetoArtCouro.Selenium.Paginas;
+
+namespace ProjetoArtCouro.Selenium.Testes
+{
+    [TestClass]
+    public class TestVenda
+    {
+        [TestMethod]
+        public void DeveCriarVendaChrome()
+        {
+            IWebDriver driver = TestSeleniumBrowsers.RealizarLoginChrome();
+            Venda vendaPage = new Venda(driver);
+            PesquisaVenda pesquisaVendaPage = new PesquisaVenda(driver);
+            vendaPage.Novo();
+            vendaPage.PreencherVenda();
+            vendaPage.Cadastrar();
+            pesquisaVendaPage.Novo();
+            pesquisaVendaPage.Cadastrar();
+            Assert.IsTrue(pesquisaVendaPage.ExisteNaListagem());
+            driver.Close();
+        }
+    }
+}
