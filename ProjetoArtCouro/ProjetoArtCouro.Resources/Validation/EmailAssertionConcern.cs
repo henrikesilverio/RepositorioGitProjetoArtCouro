7f1bf5e6baae7b744308d25f434cdfa0399b12ad
henrikesilverio@gmail.com <henrikesilverio@gmail.com>
2016-04-09 21:55:07 -0300

    Ajustando referencias do projeto resources

---
+++ ProjetoArtCouro/ProjetoArtCouro.Resources/Validation/EmailAssertionConcern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using ProjetoArtCouro.Resource.Resources;
+
+namespace ProjetoArtCouro.Resource.Validation
+{
+    public class EmailAssertionConcern
+    {
+        public static void AssertIsValid(string email)
+        {
+            if (!Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+                throw new Exception(Erros.InvalidEmail);
+        }
+    }
+}
